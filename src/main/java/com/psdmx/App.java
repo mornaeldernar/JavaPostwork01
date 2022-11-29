14583827601aff9cdd7f758056ed41741e88a06b
mornaeldernar <jimenez.rafa@gmail.com>
2022-11-28 19:46:37 -0600

    init

---
+++ src/main/java/com/psdmx/App.java
@@ -0,0 +1,15 @@
+package com.psdmx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int number = 20;
+        String binary = Integer.toBinaryString(number);
+        System.out.println("El número entero "+ number + " en binario es: " + binary);
+    }
+}
